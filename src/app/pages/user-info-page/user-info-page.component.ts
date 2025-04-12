aa13a980ec4a789afc629573438f7c6867b0f35d
Olga <113972093+Olja-tjan@users.noreply.github.com>
2025-04-12 19:06:33 +0700

    feat: add page user-info

--- src/app/pages/user-info-page/user-info-page.component.ts
+++ src/app/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IUser } from '../../utils/types';
 
 @Component({
   selector: 'app-user-info-page',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './user-info-page.component.scss'
 })
 export class UserInfoPageComponent {
+  user :IUser = {
+    id: 8,
+    email: 'lindsay.ferguson@reqres.in',
+    first_name: 'Lindsay',
+    last_name: 'Ferguson',
+    avatar: 'https://reqres.in/img/faces/8-image.jpg',
+    support: {
+      url: 'https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral'
+    },
+    text: 'Tired of writing endless social media content? Let Content Caddy generate it for you.'
+  }
 
 }
